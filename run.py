4af2132e32f18f201fb59d30f248a10dc01e1527
Carmen Alvarez Hayes <carmenalvarezhayes@gmail.com>
2022-03-23 16:27:03 -0300

    Agrego el Adivina adivinador modificado

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-a = 7
-b = 10
-suma = a - b
-print(suma)
+## Adivina adivinador....
+import random
+
+valor_aleatorio = random.randrange(101)
+gane = False
+print("Tenés 5 intentos para adivinar un entre 0 y 99")
+intento = 1
+while intento < 6 and not gane:
+    valor_ingresado = int(input('Ingresa tu número: '))
+    if valor_ingresado == valor_aleatorio:
+        print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+        gane = True
+    else:
+        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+        intento += 1
+if not gane:
+    print('\n Perdiste :(\n El número era: {}'.format(valor_aleatorio))
